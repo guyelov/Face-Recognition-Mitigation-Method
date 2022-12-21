adefca319f5c61970b8d5b6746da26354da67b29
guyelov <87119884+guyelov@users.noreply.github.com>
2022-12-21 20:11:23 +0200

    fixed streamlit.py

--- demo/streamlit.py
+++ demo/streamlit.py
@@ -24,31 +24,32 @@ if __name__ == '__main__':
     file1 = st.file_uploader("Please upload the first image", type=["jpg", "png"])
     file2 = st.file_uploader("Please upload the second image", type=["jpg", "png"])
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    mtcnn = MTCNN(image_size=112, margin=0, keep_all=True, post_process=True, device=device)
+    mtcnn = MTCNN(image_size=112, margin=0, keep_all=True, post_process=True, device=device, select_largest=True)
 
     if file1 is None or file2 is None:
         st.write('Please upload two images.')
     else:
         image1 = PIL.Image.open(file1)
         image2 = PIL.Image.open(file2)
-        transform = transforms.ToPILImage()
-        image1 = transform(mtcnn(image1)[0])
-        image2 = transform(mtcnn(image2)[0])
-        st.write(image1)
+
 
         st.image(image1, caption='Uploaded Image.', use_column_width=True)
         st.image(image2, caption='Uploaded Image.', use_column_width=True)
-        st.write("")
-        st.write("Classifying...")
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        predictor = load_predictor("demo/iresnet100_checkpoint.pth", device)
-        embedder = Embedder(device=device, model_name='iresnet100', train=False)
-        image1 = transform_image(image1)
-        image2 = transform_image(image2)
-        embedded_images = embedder(image1, image2)
-        pred = predictor(embedded_images)[0]
-        st.write(predictor(embedded_images, return_proba=True))
-        if pred == 1:
-            st.write('The two images are of the same person.')
+        image1 = mtcnn(image1)[0]
+        image2 = mtcnn(image2)[0]
+        if image1 is None or image2 is None:
+            st.write('Please upload two images of faces.')
         else:
-            st.write('The two images are of different people.')
+            st.write("Classifying...")
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            predictor = load_predictor("demo/iresnet100_checkpoint.pth", device)
+            embedder = Embedder(device=device, model_name='iresnet100', train=False)
+            image1 = transform_image(image1)
+            image2 = transform_image(image2)
+            embedded_images = embedder(image1, image2)
+            pred = predictor(embedded_images)[0]
+            st.write(predictor(embedded_images, return_proba=True))
+            if pred == 1:
+                st.write('The two images are of the same person.')
+            else:
+                st.write('The two images are of different people.')
