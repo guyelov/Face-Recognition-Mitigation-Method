e85e70f21d016a9307b62acfae707af1d78913cb
guyelov <87119884+guyelov@users.noreply.github.com>
2022-12-28 15:40:39 +0200

    fixed streamlit

--- demo/streamlit.py
+++ demo/streamlit.py
@@ -71,39 +71,11 @@ if __name__ == '__main__':
         embedded_images = embedder(image1, image2)
         pred = predictor(embedded_images)[0]
         if pred == 1:
-            st.markdown("The two images are of the same person ✅")
+            st.markdown("The two images are of the **same** person ✅",)
             # st.write('The two images are of the same person.')
         else:
             st.markdown("The two images are not of the same person ❌")
             # st.write('The two images are of different people.')
 
-        # Load the selected images from the LFW folder
-        # image1 = PIL.Image.open(os.path.join(lfw_path, file1_select, file1_image))
-        # image2 = PIL.Image.open(os.path.join(lfw_path, file2_select, file2_image))
-    # if file1 is None and file2 is None and not lfw_path:
-    #     st.write("Please upload two face images")
-    # else:
-    #     if file2 is None and not lfw_path:
-    #         st.write("Please upload the second image")
-    #     else:
-    #         # image1 = PIL.Image.open(file1)
-    #         # image2 = PIL.Image.open(file2)
-    #         #
-    #
-    #         st.image(image1, caption='Uploaded Image.', use_column_width=True)
-    #         st.image(image2, caption='Uploaded Image.', use_column_width=True)
-    #
-    #         st.write("Classifying...")
-    #         image1 = transform_image(image1)
-    #         image2 = transform_image(image2)
-    #         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    #         predictor = load_predictor("demo/iresnet100_checkpoint.pth", device)
-    #         embedder = Embedder(device=device, model_name='iresnet100', train=False)
-    #         embedded_images = embedder(image1, image2)
-    #         pred = predictor(embedded_images)[0]
-    #         st.write(predictor(embedded_images, return_proba=True))
-    #         if pred == 1:
-    #             st.write('The two images are of the same person.')
-    #         else:
-    #             st.write('The two images are of different people.')
-    st.image(logo, width=100)
+
+    # st.image(logo, width=100)
