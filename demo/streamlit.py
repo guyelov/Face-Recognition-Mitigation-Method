20d232cda8df508ae4dc3c5247f322661189e5bd
guyelov <87119884+guyelov@users.noreply.github.com>
2022-12-28 15:22:07 +0200

    fixed streamlit

--- demo/streamlit.py
+++ demo/streamlit.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         # List the subfolders in the LFW folder
         image_files = os.listdir(lfw_path)
         # Create the select box
-        selected_image = st.multiselect('Select an image:', image_files,limit=2)
+        selected_image = st.multiselect('Select an image:', image_files,max_selections =2)
         image = PIL.Image.open(os.path.join(lfw_path, selected_image))
         st.image(image)
 
